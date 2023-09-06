ae3e47d7e667d7215df050de2c489c9d39a889f0
PoulDev <83310250+PoulDev@users.noreply.github.com>
2023-09-06 22:16:27 +0200

    Host addr & port config

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, cli, redirect
 from quacks import Quacks
+from configs import configs
 from core import ACTIVE_SERVER
 
 cli.show_server_banner = lambda *args, **kwargs: None
@@ -12,4 +13,4 @@ def home():
     return redirect(ACTIVE_SERVER)
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', 5000)
+    app.run(configs['hosting']['address'], configs['hosting']['port'])
