467f4d64f067db7eda9d51830594b112b726cea6
PoulDev <filippotrabattoni@gmail.com>
2023-11-03 21:38:22 +0100

    Censure Bot

--- plugins/censure.py
+++ plugins/censure.py
@@ -2,11 +2,6 @@ from uuid import uuid4
 from src.api.plugins.events.packets.types.message import MessageEvent
 from src.api.server.game.data.chat import messageConfirm, sendMessage
 from src.api.plugins.controller import CallbacksStorage
-from src.api.plugins.events.packets import Packet2ServerEvent
-from core import logging
-
-logging.debug('[green]█[/][white]█[/][red]█[/] [green][/]')
-
 
 @CallbacksStorage.ws_event('send_message')
 def ping_command(event: MessageEvent):
@@ -18,5 +13,8 @@ def ping_command(event: MessageEvent):
         event.ignore()
         event.client.send(messageConfirm(event.packet['data'].get('req_id'), str(uuid4()), 'hide'))
 
-        event.client.send(sendMessage(str(uuid4()), "You can't say that!", 'ffff', username='Bot', color='eb4034'))
+        event.client.send(sendMessage(str(uuid4()),
+                                      "You can't say that!",
+                                      'ffff', username='Bot', color='eb4034')
+                         )
 
